6d738e646038cad951d41b92256dad35a76404fa
kauan-souza <kauanszcorreia@gmail.com>
2021-11-11 18:15:51 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/br/com/dbserver/locarimoveis/LocarImoveisApplication.java
@@ -0,0 +1,13 @@
+package br.com.dbserver.locarimoveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocarImoveisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocarImoveisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/dbserver/locarimoveis/LocarImoveisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dbserver.locarimoveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocarImoveisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
